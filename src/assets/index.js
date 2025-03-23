463dbbf332078633b3f4e66894b0906260a97951
ets-gopi <gopi.m@entitledarts.com>
2025-03-23 10:15:20 +0530

    added reume

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,20 +1,10 @@
-import logo from "./icon.jpg";
-import image from "./gImage.jpg";
-import defaultLogo from "./defaultLogo.jpg";
-import image2 from "./g1Image.jpg";
-import hatimi from "./luxury-bedroom.jpg";
-import hatimi1 from "./hatimi1.png";
-import blockchain from "./blockchain.jpg";
-import resume from "./updateResume.pdf";
-import certificateIcon from "./certificate.svg";
-export {
-  logo,
-  image,
-  defaultLogo,
-  image2,
-  hatimi,
-  blockchain,
-  hatimi1,
-  resume,
-  certificateIcon,
-};
+import logo from './icon.jpg';
+import image from './gImage.jpg';
+import defaultLogo from './defaultLogo.jpg';
+import image2 from './g1Image.jpg';
+import hatimi from './luxury-bedroom.jpg';
+import hatimi1 from './hatimi1.png';
+import blockchain from './blockchain.jpg';
+import resume from './resumeWithPhoto.pdf';
+import certificateIcon from './certificate.svg';
+export { logo, image, defaultLogo, image2, hatimi, blockchain, hatimi1, resume, certificateIcon };
